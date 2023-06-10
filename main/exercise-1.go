4da747fba6fdca0971902e24314e8b04f55a0b32
Firman Adi Nur Fatin <apclead69@gmail.com>
2023-06-10 20:59:51 +0700

    chore: remove main func & refactor exercise-1

--- main/exercise-1.go
+++ main/exercise-1.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"     // The bufio library is used to read input from the user
-	"fmt"       // The fmt library is used to print messages to the screen
-	"os"        // The os library is used to read input from the user
-	"strconv"   // The strconv library is used for data type conversion
-	"strings"   // The strings library is used to manipulate strings
+	"strconv" // The strconv library is used for data type conversion
+	"strings" // The strings library is used to manipulate strings
 )
 
 // Book is a data type that stores book information
@@ -18,8 +15,8 @@ type Book struct {
 // indexCategory is the book category in the order according to the index
 var indexCategory = []string{"6", "7", "0", "9", "4", "8", "1", "2", "5", "3"}
 
-// parseInput converts the book notation into a book object
-func parseInput(input string) Book {
+// ParseInput converts the book notation into a book object
+func ParseInput(input string) Book {
 	tokens := strings.Split(input, "")
 	category := tokens[0]
 	title := tokens[1]
@@ -31,70 +28,73 @@ func parseInput(input string) Book {
 	return Book{category, title, size}
 }
 
-func main(){
-	// Store input
-	input, inputLine := "", ""
-	var inputLineArr []string
-	
-	// Store book objects
-	var booksUnsorted []Book
-	var booksSorted []Book
+// BooksParser converts the input line into an array of book objects
+func BooksParser(inputLine string) []Book {
+	var books []Book
+	inputLineArr := strings.Split(inputLine, " ")
 
-	// Create scanner
-	scanner := bufio.NewScanner(os.Stdin)
-	
-	// Receive input
-	fmt.Println("Enter input:")
-	for scanner.Scan() {
-		input = scanner.Text()
-
-		// Stop if input is empty
-		if input == "" {
-			break
-		}
-
-		inputLine += input
-	}
-
-	inputLineArr = strings.Split(inputLine, " ")
-
-	// Loop for each inputLineArr
 	for _, notation := range inputLineArr {
-		book := parseInput(notation)
-		booksUnsorted = append(booksUnsorted, book)
+		book := ParseInput(notation)
+		books = append(books, book)
 	}
 
-	fmt.Println("Unsorted:", booksUnsorted)
-	
-	// Sort booksUnsorted based on Size
-	for i := 0; i < len(booksUnsorted); i++ {
-		for j := i + 1; j < len(booksUnsorted); j++ {
-			if booksUnsorted[i].Size > booksUnsorted[j].Size {
-				booksUnsorted[i], booksUnsorted[j] = booksUnsorted[j], booksUnsorted[i]
+	return books
+}
+
+// BookSorter sort the books based on the category and size
+func BookSorter(unsorted []Book) []Book {
+	var sorted []Book
+
+	// Sort unsorted Book based on Size
+	for i := 0; i < len(unsorted); i++ {
+		for j := i + 1; j < len(unsorted); j++ {
+			if unsorted[i].Size > unsorted[j].Size {
+				unsorted[i], unsorted[j] = unsorted[j], unsorted[i]
 			}
 		}
 	}
 
-	// Reverse the order of booksUnsorted
-	for i := 0; i < len(booksUnsorted)/2; i++ {
-		j := len(booksUnsorted) - i - 1
-		booksUnsorted[i], booksUnsorted[j] = booksUnsorted[j], booksUnsorted[i]
+	// Reverse the order of unsorted
+	for i := 0; i < len(unsorted)/2; i++ {
+		j := len(unsorted) - i - 1
+		unsorted[i], unsorted[j] = unsorted[j], unsorted[i]
 	}
 
 	// Loop for each indexCategory
 	for _, category := range indexCategory {
 
 		// Loop for each book in booksUnsorted
-		for _, book := range booksUnsorted {
-		
+		for _, book := range unsorted {
+
 			// If the category is the same as the book category
 			if category == book.Category {
 				// Add the Book to booksSorted
-				booksSorted = append(booksSorted, book)
+				sorted = append(sorted, book)
 			}
 		}
 	}
+	return sorted
+}
 
-	fmt.Println("Sorted:", booksSorted)
-	
+// StringToSortedBook converts the input string into a sorted array of book objects
+func StringToSortedBook(input string) []Book {
+	booksUnsorted := BooksParser(input)
+	booksSorted := BookSorter(booksUnsorted)
+	return booksSorted
+}
+
+// DisplayBooks converts the array of book objects into a string
+func DisplayBooks(books []Book) string {
+	var result string
+	for _, book := range books {
+		result += book.Category + book.Title + strconv.Itoa(book.Size) + " "
+	}
+	return result
+}
+
+// SortTheBooks is a function that sorts the books
+func SortTheBooks(input string) string {
+	booksSorted := StringToSortedBook(input)
+	result := DisplayBooks(booksSorted)
+	return result
 }
